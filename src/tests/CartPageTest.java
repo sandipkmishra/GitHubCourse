0089dc0e08ec24c37f61c9c96357c848327355fd
sandipkmishra <mishra.sandipkumar@gmail.com>
2021-11-07 03:03:16 +0530

    updated Cart Page

--- src/tests/CartPageTest.java
+++ src/tests/CartPageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 public class CartPageTest {
 public void cartTest() {
-	System.out.println("true");
+	System.out.println("cart page");
+	System.out.println("cart page feature");
 }
 }
