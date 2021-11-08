bffadfe6a4f5f2d0eeef3dd30b15e8628b47cbc1
sandipkmishra <mishra.sandipkumar@gmail.com>
2021-11-08 17:08:50 +0530

    added order page feature

---
+++ src/Pages/OrderPage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class OrderPage {
+public void doOrder() {
+	System.out.println("Order Page");
+}
+}
---
+++ src/tests/OrderPageTest.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class OrderPageTest {
+public void orderTest() {
+	System.out.println(true);
+}
+}
